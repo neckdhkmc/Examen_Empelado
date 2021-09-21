623a3397b7eb06d261f66e106ca31bca3cacbd68
neckdhkmc <71038665+neckdhkmc@users.noreply.github.com>
2021-09-20 20:20:48 -0500

    20/09/2021

--- Examen_Empelado/App_Start/RouteConfig.cs
+++ Examen_Empelado/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Examen_Empelado
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Empelado", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
